4d48ddff10db184f9ab0e1c36c2be554f26c12a3
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2024-09-29 12:26:56 +0000

    nav elements
    added nav elements to get navlinks instead of hard coding them

--- src/Components/Layout/Navbar.jsx
+++ src/Components/Layout/Navbar.jsx
@@ -2,6 +2,10 @@ import { FaGithub } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+  const navElements = [
+    { link: '/', linkName: 'Home' },
+    { link: '/', linkName: 'About' },
+  ]
   return (
     <nav className='navbar mb-12 shadow-lg bg-neutral text-white'>
       <div className='container mx-auto'>
@@ -10,6 +14,19 @@ const Navbar = () => {
           <Link to='/' className='text-lg font-bold align-middle '>
             Github finder
           </Link>
+        </div>
+        <div className='flex-1 px-2 mx-2'>
+          <div className='flex justify-end'>
+            {navElements.map((element, i) => (
+              <Link
+                key={i}
+                to={element.link}
+                className='btn btn-sm btn-ghost rounded-btn'
+              >
+                {element.linkName}
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </nav>

581f523076917dd9292285dd747b75a4f0de39f7
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2024-09-30 02:53:37 +0000

    About and NotFound pages
    added the about page and Not found pages to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import NotFound from './Pages/NotFound'
 import Navbar from './Components/Layout/Navbar'
 import Footer from './Components/Layout/Footer'
 
@@ -8,7 +11,14 @@ const App = () => {
       <div className='flex flex-col justify-between h-screen'>
         <Navbar />
 
-        <main>content</main>
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/notfound' element={<NotFound />} />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
+        </main>
 
         <Footer />
       </div>
--- src/Components/Layout/Navbar.jsx
+++ src/Components/Layout/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   const navElements = [
     { link: '/', linkName: 'Home' },
-    { link: '/', linkName: 'About' },
+    { link: '/about', linkName: 'About' },
   ]
   return (
     <nav className='navbar mb-12 shadow-lg bg-neutral text-white'>

27aede03f383aeb4d86c4ed8cb90c3a55bcb998d
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2024-10-06 18:13:24 +0000

    single user page setup

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -8,6 +8,7 @@ const GITHUB_URL = 'https://api.github.com'
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
+    user: {},
     loading: false,
   }
 
@@ -31,6 +32,24 @@ export const GithubProvider = ({ children }) => {
     })
   }
 
+  //get single user
+  const getUser = async (login) => {
+    setLoaing()
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`)
+
+    if (response.status === 404) {
+      window.location = '/notfound'
+    } else {
+      const data = await response.json()
+
+      dispatch({
+        type: 'GET_USER',
+        payload: data,
+      })
+    }
+  }
+
   // clear users from state
   const clearUsers = () =>
     dispatch({
@@ -45,8 +64,10 @@ export const GithubProvider = ({ children }) => {
       value={{
         users: state.users,
         loading: state.loading,
+        user: state.user,
         searchUsers,
         clearUsers,
+        getUser,
       }}
     >
       {children}

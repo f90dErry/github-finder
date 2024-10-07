f7061c0fa1c815406df04cd034d9c70aeeb234fd
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2024-10-07 01:07:04 +0000

    Update User.jsx

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,9 +1,12 @@
+import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import Spinner from '../components/layout/Spinner'
 import { useEffect, useContext } from 'react'
 import { useParams } from 'react-router-dom'
 import GithubContext from '../context/github/GithubContext'
 
 const User = () => {
-  const { getUser, user } = useContext(GithubContext)
+  const { getUser, user, loading } = useContext(GithubContext)
 
   const params = useParams()
 
@@ -11,7 +14,46 @@ const User = () => {
     getUser(params.login)
   }, [])
 
-  return <div>user</div>
+  const {
+    name,
+    type,
+    avartar_url,
+    location,
+    blog,
+    twitter_username,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gist,
+    hirable,
+  } = user
+
+  if (loading) {
+    return <Spinner />
+  }
+
+  return (
+    <>
+      <div className='w-full mx-auto lg:w-10/12'>
+        <div className='mb-4'>
+          <Link to='/' className='btn btn-ghost'>
+            Back To Search
+          </Link>
+        </div>
+        <div className='grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8'>
+          <div className='custom-card-image mb-6 md:mb-0'>
+            <div className='rounded-lg shadow-xl card image-full'>
+              <figure>
+                <img src={avartar_url} alt='user_img' />
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default User
